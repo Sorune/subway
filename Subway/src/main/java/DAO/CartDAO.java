7c8ed12bfa7c8dff4eb14eeb9e878671ea2fad76
yangjiwung <5875130>
2024-02-16 18:11:29 +0900

    초기1

--- Subway/src/main/java/DAO/CartDAO.java
+++ Subway/src/main/java/DAO/CartDAO.java
@@ -1,60 +1,16 @@
 package DAO;
 
-import java.sql.SQLException;
-
-import DTO.CartDTO;
 import util.JDBConnect;
 
-public class CartDAO extends JDBConnect {
+public class CartDAO extends JDBConnect{
 
-	public void readCart() {
-		// 재작 중 다른분들 DTO,DAO를 이용해 만들 예정
-	}
-
-	// 결제정보를 모두 담음
-	public int paymentInfo(CartDTO dto) {
-		int result = 0;
-		String sql = "insert into cart values(?,?,?,?,?,?,?,?,?,?,?,?)";
-
-		try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, dto.getOrder_num());
-			pstmt.setInt(2, dto.getMenu_order());
-			pstmt.setString(3, dto.getOrder_kind());
-			pstmt.setString(4, dto.getPay_kind());
-			pstmt.setString(5, dto.getMenu_name());
-			pstmt.setInt(6, dto.getMenu_qty());
-			pstmt.setString(7, dto.getMenu_topping()); // int인지 String인지 불확실
-			pstmt.setInt(8, dto.getMenu_total_price());
-			pstmt.setString(9, dto.getUser_id());
-			pstmt.setString(10, dto.getUser_name());
-			pstmt.setString(11, dto.getUser_add());
-			pstmt.setString(12, dto.getUser_phone());
-
-			// JSFunction용 정수값 반환
-			result = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			result = 0;
-			e.printStackTrace();
-			System.out.println("");
-		}
-		return result;
-
-	}
-
-	public void close() {
-
-		try {
-			if (rs != null)		rs.close();
-			if (stmt != null)	stmt.close();
-			if (pstmt != null)	pstmt.close();
-			if (con != null)	con.close();
-			System.out.println("JDBC Disconnect Sucess");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("close() 예외 발생 : JDBC 드라이버 연결 해제 중 오류 발생");
-		}
-
+	
+	public void OderLists(String user_id) {
+		
+		String sql = "select * from CART, MENU_ORDER where cart.user_id = menu_order.user_id";
+		
+		//pstmt = con.prepareStatement(sql); 
+		
+		
 	}
 }
--- Subway/src/main/webapp/Cart.jsp
+++ Subway/src/main/webapp/Cart.jsp
@@ -1,34 +1,30 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>장바구니</title>
+<!DOCTYPE html><html><head><meta charset="UTF-8">
+<title>Cart</title>
 </head>
 <body>
-	<jsp:include page="./menuList.jsp" />
-	
+
 	<h2>장바구니</h2>
 	
 	<!-- 장바구니 목록 테이블 -->
-	<form id="paymenyform" class="mb-3" action="PaymentProcess.jsp" method="post">
-	
 	<table border="1" width="90%">
 		<tr>
 			<th width="*">메뉴명</th>
 			<th width="8%">수량</th>
 			<th width=15%>가격</th>
+			<th width=20%></th>
 		</tr>
 	</table>
 	
 	<!-- 결제 버튼 -->
 	<table border="1" width="90%">
 		<tr align="center" >
-			<td width="100" colspan="8"><button type="submit" name ="payment">결제하기</button>
-			<button type="reset" onclick="location.href='DeleteCart.jsp'">장바구니 비우기</button></td>
+			<td width="100" colspan="8"><button type="submit" name ="payment" onclick="location.href='./Payment.jsp';">결제하기</button>
+			<td align="right"><button type="reset" onclick="location.href='DeleteCart.jsp'">장바구니 비우기</button></td>
 		</tr>
 	</table>	
-	</form>
+	
+
 </body>
 </html>
